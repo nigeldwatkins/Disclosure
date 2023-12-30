8feceb45237a31738ee6027f3bd90466ec7303cc
Nigel D. Watkins <nigeldwatkins@gmail.com>
2023-12-29 21:33:57 -0500

    Working on logout code

--- userroutes/logout.php
+++ userroutes/logout.php
@@ -4,16 +4,11 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { 
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type");
-exit();
-}
 
 require_once(__DIR__ . "/../Manager/user.php");
 require_once(__DIR__ . "/../config/mysql.php");
 
-
+session_start();
 
 // Handles POST request for logging out
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -25,6 +20,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Returns success response 
     http_response_code(200);
     echo json_encode(['message' => 'User logged out successfully']);
+    exit();
+} else {
+    // Handle unsupported HTTP methods
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(['message' => 'Method not allowed']);
     exit();
 }
 ?>
